2077c5b7d8b08d6e16f61c8cca44b5869ef678e3
FredBSZ <146657102+FredBSZ@users.noreply.github.com>
2023-10-01 18:47:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header 'Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit. text ('O Kale, Spinach & Rocket Smoothie')
+streamlit.text ('O Kale, Spinach & Rocket Smoothie')
 streamlit.text ('<) Hard-Boiled Free-Range Egg')
 streamlit.text('DO Avocado Toast')
